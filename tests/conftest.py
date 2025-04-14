f86d04751820b8302b63263cda38cc9d9456cc86
corka149 <corka149@mailbox.org>
2025-04-14 16:33:44 +0200

    Add more testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,3 +25,55 @@ def db_setup():
 @pytest.fixture
 def client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture
+def db_session():
+    import gallery.config as config
+    import gallery.db as db
+
+    gallery_config = config.get_config()
+    db.init(gallery_config)
+
+    session = next(db.session())
+    
+    yield session
+    
+    session.close()
+
+
+@pytest.fixture
+def image_service(db_session):
+    from gallery.service import ImageService
+
+    return ImageService(session=db_session)
+
+
+@pytest.fixture
+def user_service(db_session):
+    from gallery.service import UserService
+
+    return UserService(session=db_session)
+
+
+@pytest.fixture
+def auth_service(user_service):
+    import gallery.config as config
+    from gallery.service import AuthService
+    
+    gallery_config = config.get_config()
+
+    return AuthService(user_service=user_service, config=gallery_config)
+
+
+@pytest.fixture
+def image_dir(tmp_path):
+    import gallery.config as config
+
+    image_dir = tmp_path / "images"
+    image_dir.mkdir(parents=True, exist_ok=True)
+    
+    gallery_config = config.get_config()
+    gallery_config.image_directory = str(image_dir)
+    
+    return image_dir
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,24 @@
 
 from fastapi.testclient import TestClient
 
+from gallery import db
+from gallery.service import ImageService, UserService
 
-def test_home_without_auth(db_setup, client: TestClient):
+
+def test_home_without_auth(db_setup, client: TestClient, image_service: ImageService):
     response = client.get("/")
     assert response.is_success
     assert response.headers["content-type"] == "text/html; charset=utf-8"
     assert "Gallery" in response.text
+    assert "Anmelden" in response.text
+
+
+def test_login(db_setup, client: TestClient, user_service: UserService):
+    user = db.User(username="some_user", email="some_user@example.xyz")
+    password = "some_password"
+    user_service.save(user, password)
+    
+    response = client.post("/login", data={"username": user.username, "password": password})
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert "Abmelden" in response.text
